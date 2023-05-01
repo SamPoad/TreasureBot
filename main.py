b659efc790ac9011ff162901afbb397a9d54cf3d
Sam Poad <sam.poad@gmail.com>
2023-04-30 21:17:13 -0400

    more poking at it with CoPilot

--- main.py
+++ main.py
@@ -19,13 +19,26 @@ print(gem_df.head())
 combo_df = pd.DataFrame.from_dict(gem_combinations, orient = 'index', columns= ['number_of_gems', 'number_of_colors', 'multiplier'])
 print(combo_df.head())
 
+# test dictionry for the user's treasure
+user_treasure = { # 'name' : quantity,
+    'Elegant Mask' : 1,
+}
+
+# test dictionary for the user's gems
+user_gems = { # 'name' : quantity, 
+    'Ruby' : 1,
+    'Sapphire' : 1,
+    'Yellow Diamond' : 1,
+} 
 
 
 
+# based on the test data, the response should be to slot one of each gem into the Elegant Mask, and then sell it for 5000(base value) + (ruby) + (sapphire) + (yellow diamond) , and then use the Three Colors multiplier of 1.3 to get the final value of 5000 + 3000 + 4000 + 7000 = 19000 * 1.3 = 24700
 # GET INPUT FROM THE USER ON THEIR TREASURE AND GEM ITEMS 
 
 # calculate the possible combinations of gems and treasure
 
+# filter the possible combinations based on the user's inventory
 
 
 # look at the treasure, gem, and combo dataframes and find the max value combination
